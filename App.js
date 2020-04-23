92a16073e5ca61cbc147f81705e3d0ba5b6816e2
Parth <parthshahp1@gmail.com>
2020-04-23 19:30:56 -0400

    This is a test

--- App.js
+++ App.js
@@ -5,6 +5,10 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello</Text>
+      <Button
+        title="Learn More"
+      />
     </View>
   );
 }
