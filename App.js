ee0d471f4e0c5541e0cf73aaffd43113045fdcc2
Dhananjay Khazanchi <dkhazanchi1@gsu.edu>
2020-04-23 19:23:10 -0400

    more firebase stuff

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './components/Login';
+import Apikeys from './constants/apikeys';
+import * as firebase from 'firebase';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      
+
       <Login />
     </View>
   );
+}
+
+constructor(props){
+  super(props);
+  this.state = {
+    isLoadingComplete: false;
+  };
+
+  //Intializing Firebase
+  if(!firebase.apps.length){ firebase.intializeApp(Apikeys.FirebaseConfig);}
 }
 
 const styles = StyleSheet.create({
