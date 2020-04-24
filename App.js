afd6389772ba2a1a69f3b45ba5999a7c3285fe57
Dhananjay Khazanchi <dkhazanchi1@gsu.edu>
2020-04-23 20:01:51 -0400

    removing some firebase stuff that was causing issues

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './components/Login';
-import Apikeys from './constants/apikeys';
 import * as firebase from 'firebase';
 
 
@@ -12,16 +11,6 @@ export default function App() {
       <Login />
     </View>
   );
-}
-
-constructor(props){
-  super(props);
-  this.state = {
-    isLoadingComplete: false;
-  };
-
-  //Intializing Firebase
-  if(!firebase.apps.length){ firebase.intializeApp(Apikeys.FirebaseConfig);}
 }
 
 const styles = StyleSheet.create({
