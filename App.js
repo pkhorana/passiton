1f459144dd4663c5dbf6af86871d393a0769fff8
Parth <parthshahp1@gmail.com>
2020-04-23 20:08:39 -0400

    Fixed my App.js

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './components/Login';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello</Text>
+
+      <Login />
     </View>
   );
 }
