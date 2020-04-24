2f030209c2edce26a4bbc7ff4faec4c08ffede32
Dhananjay Khazanchi <dkhazanchi1@gsu.edu>
2020-04-23 20:54:26 -0400

    remove firebase import

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './components/Login';
-import * as firebase from 'firebase';
 
 
 export default function App() {
