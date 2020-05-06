67ff494fa85b700d02180740c57462348d0b8421
pkhorana <pranavkhorana@gmail.com>
2020-05-05 23:47:57 -0400

    added google sign in

--- components/Loading.js
+++ components/Loading.js
@@ -8,20 +8,45 @@ export default function Loading(props) {
     const usersRef = firebase.database().ref().child('users');
     var p = null;
 
+
+    function writeLoginCredentials(emailText) {
+        var user = firebase.auth().currentUser;
+        var myRef = usersRef.child(user.uid);
+        var data = 
+        {
+            email: emailText,
+            profileComplete: 'No',
+        }
+        myRef.set(data);
+      }
+
     useEffect(() => {
         firebase.auth().onAuthStateChanged(user => {
+            
             if (user && user.emailVerified) {
-                
+
 
                 usersRef.child(user.uid).child('profileComplete').once('value').then(function(snapshot) {
                     p = snapshot.val();
                 }).then( () => {
                         if (p === 'No') {
                             props.navigation.navigate('CreateProfileScreen');
-                        } else {
+                        }
+                        else if (p == null) {
+                            usersRef.once('value').then(function(snapshot) {
+                                if (!snapshot.hasChild(user.uid)) {
+                                    writeLoginCredentials(user.email);
+                                }
+                            }).then(props.navigation.navigate('CreateProfileScreen'));
+                        } 
+                        else {
                             props.navigation.navigate('HomeScreen');
                         }
                 });
+                
+                
+
+                
             } else {
                 props.navigation.navigate('LoginScreen');
             } 
--- components/Login.js
+++ components/Login.js
@@ -1,17 +1,99 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect} from 'react';
 import {StyleSheet, Button, TextInput, Text, View} from 'react-native';
 import * as firebase from 'firebase';
+import * as Google from 'expo-google-app-auth';
+
 
 
 export default function Login(props) {
 
+  const androidID = '726496770670-po97qe023h1g4ursm0ubccm1rliad5o3.apps.googleusercontent.com';
+  const iosID = '726496770670-6errsd2kf47u6hvusupe5skgmmqg8uth.apps.googleusercontent.com';
 
   
   const usersRef = firebase.database().ref().child('users');
 
   const [userText, setUser] = useState('');
   const [passText, setPass] = useState('');
-  const [userKey, setKey] = useState('');
+
+  
+
+  
+
+  async function signInWithGoogleAsync() {
+    try {
+      const result = await Google.logInAsync({
+        androidClientId: androidID,
+        iosClientId: iosID,
+        scopes: ['profile', 'email'],
+      });
+      
+      
+  
+      if (result.type === 'success') {
+        onSignIn(result);
+        return result.accessToken;
+      } else {
+        
+        return { cancelled: true };
+      }
+    } catch (e) {
+    
+      return { error: true };
+      
+    }
+    
+  }
+
+  
+  function onSignIn(googleUser) {
+    console.log('Google Auth Response', googleUser);
+    // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+    
+    var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
+      unsubscribe();
+      
+      // Check if we are already signed-in Firebase with the correct user.
+      if (!isUserEqual(googleUser, firebaseUser)) {
+        // Build Firebase credential with the Google ID token.
+        var credential = firebase.auth.GoogleAuthProvider.credential(
+            // googleUser.getAuthResponse().id_token
+            googleUser.idToken,
+            googleUser.accessToken
+        );
+        // Sign in with credential from the Google user.
+        firebase.auth().signInWithCredential(credential).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          
+          
+          // ...
+        });
+      } else {
+        console.log('User already signed-in Firebase.');
+        
+      }
+    });
+  }
+
+  function isUserEqual(googleUser, firebaseUser) {
+    if (firebaseUser) {
+      var providerData = firebaseUser.providerData;
+      for (var i = 0; i < providerData.length; i++) {
+        if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+            providerData[i].uid === googleUser.getBasicProfile().getId()) {
+          // We don't need to reauth the Firebase connection.
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 
 
   function successLogin() {
@@ -69,6 +151,10 @@ export default function Login(props) {
         />
         <Button
             title = "Login with Facebook"
+        />
+        <Button
+            title = "Login with Google"
+            onPress={() => signInWithGoogleAsync()}
         />
 
         <Button
