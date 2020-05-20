2e7faaea63fe94a01cc0e7523d8d62b0f1632359
pkhorana <pranavkhorana@gmail.com>
2020-05-20 00:39:56 -0400

    updated react navigation and got drawer to work

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-import {createAppContainer, createSwitchNavigator} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+// import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+// import {createStackNavigator} from 'react-navigation-stack';
+// import {createDrawerNavigator} from 'react-navigation-drawer';
+
+
 
 import Loading from './components/Loading';
 import Login from './components/Login';
@@ -15,64 +21,105 @@ import Home from './components/Home';
 import * as firebase from 'firebase';
 import {firebaseConfig} from './config';
 
+
+
 firebase.initializeApp(firebaseConfig);
+
+const AuthStack = createStackNavigator();
+const HomeStack =  createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+
+
+
+
+const HomeStackScreen = () => (
+  <HomeStack.Navigator >
+  <HomeStack.Screen name="HomeScreen" component={Home}/>
+  </HomeStack.Navigator>
+);
+
+
+const DrawerNavigatorScreen = () => (
+  <Drawer.Navigator>
+    <Drawer.Screen name="Home" component={HomeStackScreen} />
+  </Drawer.Navigator>
+);
+
+
+
 
 
 export default function App() {
   return (
-    <AppContainer/>  
+    // <AppContainer/> 
+    
+    <NavigationContainer>
+      <AuthStack.Navigator>
+        <AuthStack.Screen name="LoadingScreen" component={Loading} options={{title: ''}}/>
+        <AuthStack.Screen name="LoginScreen" component={Login} options={{title: 'Login'}} />
+        <AuthStack.Screen name="CreateAccountScreen" component={CreateAccount} options={{title: 'Create Account'}}/>
+        <AuthStack.Screen name="VerifyAccountScreen" component={VerifyAccount} options={{title: ''}}/>
+        <AuthStack.Screen name="ForgotPasswordScreen" component={ForgotPassword} options={{title: 'Forgot Password'}}/>
+        <AuthStack.Screen name="CreateProfileScreen" component={CreateProfile} options={{title: 'Create Profile'}}/>
+        <AuthStack.Screen name="HomeScreen" component={DrawerNavigatorScreen} options={{headerShown: false}}/>
+
+      </AuthStack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
 
-const StackNav = createStackNavigator( 
-  {
-      HomeScreen: {
-          screen: Home,
-          // navigationOptions: 
-          // {
-          //     title: 'Home',
-          //     headerStyle: {
-          //         backgroundColor: '#f4511e',
-          //     },
-          //     headerTintColor: '#fff',
-          //     headerTitleStyle: {
-          //         fontWeight: 'bold',
-          //     },
-          // }
-      }
-  },
-  {
-      initialRouteName: 'HomeScreen'
-  }
-);
+// const DrawerNav = createDrawerNavigator(
+//   {
+//     Home: Home,
+//   },
+//   {
+//     initialRouteName: 'Home',
+//     drawerPosition: 'left',
+//   }
+// );
 
 
-const SwitchNav = createSwitchNavigator(
-  {
-  LoadingScreen: Loading,
-  LoginScreen: Login,
-  CreateAccountScreen: CreateAccount,
-  VerifyAccountScreen: VerifyAccount,
-  ForgotPasswordScreen: ForgotPassword,
-  CreateProfileScreen: CreateProfile,
-  HomeScreen: StackNav,
-  },
-  {
-    initialRouteName: 'LoadingScreen',
-  }
-);
-
-const AppContainer = createAppContainer(SwitchNav);
+// const StackNav = createStackNavigator( 
+//   {
+//       HomeScreen: Home,
+//       DrawerToggle: DrawerNav
+          
+//   },
+//   {
+//       initialRouteName: 'HomeScreen'
+//   }
+// );
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+// const SwitchNav = createSwitchNavigator(
+//   {
+//   LoadingScreen: Loading,
+//   LoginScreen: Login,
+//   DrawerToggle: DrawerNav,
+//   CreateAccountScreen: CreateAccount,
+//   VerifyAccountScreen: VerifyAccount,
+//   ForgotPasswordScreen: ForgotPassword,
+//   CreateProfileScreen: CreateProfile,
+//   HomeScreen: StackNav,
+//   },
+//   {
+//     initialRouteName: 'LoadingScreen',
+//   }
+// );
+
+// const AppContainer = createAppContainer(SwitchNav);
 
 
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+
+
+// });
--- components/Home.js
+++ components/Home.js
@@ -1,17 +1,50 @@
 import React, {useState, useEffect} from 'react';
-
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import {Text, View, TouchableOpacity, ScrollView} from 'react-native';
 import * as firebase from 'firebase';
 import styles from './Styles';
 
+
+
 export default function Home(props) {
+
+    props.navigation.setOptions ( {
+            title: 'Home',
+            headerTitleAlign: 'center',
+            headerStyle: {
+                backgroundColor: '#4169e1',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold',
+            },
+            headerLeft: () => (
+                <TouchableOpacity onPress={() => {
+                    props.navigation.toggleDrawer();
+                }}>
+                <Text>MENU</Text>
+                </TouchableOpacity>
+            )
+    });
+
+    const categoryNames = ["Video Games", "Food/Beverages", "Entertainment", "Sports", "Fashion", "Politics"];
+    const categoryComponents = categoryNames.map((category, i) => <TouchableOpacity
+                  key = {i}
+                  style={styles.button}>
+                  <Text>{category}</Text>
+                  </TouchableOpacity>)
 
     const [currUser, setCurrUser] = useState(null);
     const usersRef = firebase.database().ref().child('users');
+    const [fName, setFName] = useState('');
+    const user = firebase.auth().currentUser;
 
     useEffect(() => {
        setCurrUser(firebase.auth());
     });
+
+
+
 
     function signOut() {
         firebase
@@ -20,45 +53,58 @@ export default function Home(props) {
         .then(() => props.navigation.navigate('LoginScreen'));
     }
 
+    usersRef.child(user.uid).child('fName').once('value').then(function(snapshot) {
+        setFName(snapshot.val());
+        console.log(fName);
+    })
+
     return (
-        <View style={styles.container}>
-        <View style={styles.entryContainer}>
-       
-        <ScrollView >
         
-        <Text style={styles.title}>Welcome to Home Screen</Text>
+        <View style={styles.container}>
+        <ScrollView>
+        
+        <Text style={styles.introMessage}>Welcome to the Home Screen, {fName}!</Text>
+
+        {/* <FlatList
+            data={categoryNames}
+            renderItem={({category, index}) => (
+
+            )}
+        /> */}
+        <>{categoryComponents}</>
         <TouchableOpacity style={styles.button}
             onPress={() => signOut()}>
             <Text>LOGOUT</Text>
         </TouchableOpacity>
         </ScrollView >
         </View>
-        </View>
     );
 }
 
 
-Home.navigationOptions = ({ navigation }) => {
-    return {
-        title: 'Home',
-        headerTitleAlign: 'center',
-        headerStyle: {
-            backgroundColor: '#00008b',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-            fontWeight: 'bold',
-        },
-        headerLeft: () => (
-            <TouchableOpacity onPress={() => navigation.navigate('CreateProfileScreen')}>
-            <Text>MENU</Text>
-            </TouchableOpacity>
-        )
+// Home.navigationOptions = ({ navigation }) => {
+//     return {
+//         title: 'Home',
+//         headerTitleAlign: 'center',
+//         headerStyle: {
+//             backgroundColor: '#00008b',
+//         },
+//         headerTintColor: '#fff',
+//         headerTitleStyle: {
+//             fontWeight: 'bold',
+//         },
+//         headerLeft: () => (
+//             <TouchableOpacity onPress={() => {
+//                 console.log('1');
+//             }}>
+//             <Text>MENU</Text>
+//             </TouchableOpacity>
+//         )
 
-    }
+//     }
             
         
-}
+// }
 
 
 
